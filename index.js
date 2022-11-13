6794880e2ef956b5dfedc31bf64403c3a1c60f4a
sajid_f57ovgd <sajidhasan2641@gmail.com>
2022-11-13 13:23:30 -0600

    gg

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 let row = 3;
 let column = 3;
+let size = 3;
 let currentCard;
 let otherCard;
 let parentofFill;
 let swapElement;
 let doesExit;
 let turn = 0;
+handleInput();
 const imageOrder = [
   "images/1",
   "images/2",
@@ -17,7 +19,6 @@ const imageOrder = [
   "images/8",
   "images/9",
 ];
-
 for (let i = 0; i < imageOrder.length; i++) {
   let j = Math.floor(Math.random() * imageOrder.length);
 
@@ -34,56 +35,127 @@ window.onload = () => {
       cards.id = r.toString() + "-" + c.toString();
       cards.src = imageOrder.shift() + ".jpg";
       //Draging Funtionality
-      cards.addEventListener("dragstart", dragStart);
-      cards.addEventListener("dragover", dragOver);
-      cards.addEventListener("dragenter", dragEnter);
-      cards.addEventListener("dragleave", dragLeave);
-      cards.addEventListener("drop", dragDrop);
-      cards.addEventListener("dragend", dragEnd);
+      // cards.addEventListener("dragstart", dragStart);
+      // cards.addEventListener("dragover", dragOver);
+      // cards.addEventListener("dragenter", dragEnter);
+      // cards.addEventListener("dragleave", dragLeave);
+      // cards.addEventListener("drop", dragDrop);
+      // cards.addEventListener("dragend", dragEnd);
 
       document.getElementById("board").append(cards);
     }
   }
 };
-
-//Creating functions of Drag
-function dragStart() {
-  currentCard = this;
-  // parentofFill = currentCard.parentNode;
-  setTimeout(() => {
-    return (this.style.visibility = "hidden");
-  }, 0);
+function handleInput() {
+  document.addEventListener("keydown", handleKeyDown);
 }
-
-function dragOver(e) {
-  e.preventDefault();
-}
-function dragEnter(e) {
-  e.preventDefault();
-  if (this.querySelector("img") !== null) {
-    doesExit = true;
-    const element = this.querySelectorAll("img");
-    swapElement = element[0];
-  } else {
-    doesExit = false;
+function handleKeyDown(e) {
+  // console.log(e.key);
+  switch (e.key) {
+    case "ArrowLeft":
+      moveLeft();
+      break;
+    case "ArrowRight":
+      moveRight();
+      break;
+    case "ArrowUp":
+      moveUp();
+      break;
+    case "ArrowDown":
+      moveDown();
+      break;
   }
 }
-function dragLeave() {}
-function dragDrop() {
-  otherCard = this;
 
-  currentCard.style.visibility = "visible";
-  if (doesExit) {
-    parentofFill.append(otherCard);
+function moveLeft() {
+  const emptyPuzzle = getEmptyPuzzle();
+  const rightPuzzle = getRightPuzzle();
+  console.log(emptyPuzzle, rightPuzzle);
+}
+function moveRight() {}
+function moveUp() {}
+function moveDown() {}
+
+function getRightPuzzle() {
+  const emptyPuzzle = getEmptyPuzzle();
+  const isRightEdge = emptyPuzzle.imageOrder === size;
+  if (isRightEdge) {
+    return null;
   }
-  parentofFill.children[1].value = "";
-  this.append(otherCard);
+  const puzzle = getPuzzlebyPos(emptyPuzzle.imageOrder + 1);
+  return puzzle;
 }
-function dragEnd() {
-  let currentImg = currentCard.src;
-  let otherImg = otherCard.src;
-  currentCard.src = otherImg;
-  otherCard.src = currentImg;
-  turns += 1;
-  document.getElementById("turns").innerText = turns;
+function getLeftPuzzle() {
+  const emptyPuzzle = getEmptyPuzzle();
+  const isLeftEdge = emptyPuzzle.imageOrder === 1;
+  if (isLeftEdge) {
+    return null;
+  }
+  const puzzle = getPuzzlebyPos(emptyPuzzle.imageOrder - 1);
+  return puzzle;
 }
+function getTopPuzzle() {
+  const emptyPuzzle = getEmptyPuzzle();
+  const isTopEdge = emptyPuzzle.imageOrder === 1;
+  if (isTopEdge) {
+    return null;
+  }
+  const puzzle = getPuzzlebyPos(emptyPuzzle.imageOrder - row);
+  return puzzle;
+}
+function getBottomPuzzle() {
+  const emptyPuzzle = getEmptyPuzzle();
+  const isBottomEdge = emptyPuzzle.imageOrder === row;
+  if (isBottomEdge) {
+    return null;
+  }
+  const puzzle = getPuzzlebyPos(emptyPuzzle.imageOrder + 1);
+  return puzzle;
+}
+
+function getEmptyPuzzle() {
+  return imageOrder.find((item) => item.imageOrder["images/3.jpg"]);
+}
+function getPuzzlebyPos(pos) {
+  return imageOrder.find((item) => item.imageOrder === pos);
+}
+// //Creating functions of Drag
+// function dragStart() {
+//   currentCard = this;
+//   setTimeout(() => {
+//     return (this.style.visibility = "hidden");
+//   }, 0);
+// }
+
+// function dragOver(e) {
+//   e.preventDefault();
+// }
+// function dragEnter(e) {
+//   e.preventDefault();
+//   if (this.querySelector("img") !== null) {
+//     doesExit = true;
+//     const element = this.querySelectorAll("img");
+//     swapElement = element[0];
+//   } else {
+//     doesExit = false;
+//   }
+// }
+// function dragLeave() {}
+// function dragDrop() {
+//   otherCard = this;
+
+//   currentCard.style.visibility = "visible";
+//   if (doesExit) {
+//     parentofFill.append(otherCard);
+//   }
+//   parentofFill.children[1].value = "";
+//   this.append(otherCard);
+// }
+// function dragEnd() {
+//   let currentImg = currentCard.src;
+//   let otherImg = otherCard.src;
+//   currentCard.src = otherImg;
+//   otherCard.src = currentImg;
+//   turns += 1;
+//   document.getElementById("turns").innerText = turns;
+// }
